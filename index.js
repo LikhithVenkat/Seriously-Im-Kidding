37b5b35741703177fb8f663777c1af26b39051e4
Likhith Venkat Pagadala <100864251+LikhithVenkat@users.noreply.github.com>
2023-09-22 21:20:56 +0530

    Updated cors

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const path = require('path')
-const express = require('express')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 1812
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors'); 
+const port = process.env.PORT || 1812;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use('/openai', require('./routes/openaiRoutes'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/openai', require('./routes/openaiRoutes'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-    console.log('Server on 1812')
-})
+  console.log('Server on 1812');
+});
